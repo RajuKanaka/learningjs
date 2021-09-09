0a12c1f9066d161c4bd655e9a12a8f4f0096237f
Kanaka Raju <vkanaka.1213@gmail.com>
2021-09-09 18:53:32 +0530

    learning variables and scope

---
+++ variables.js
@@ -0,0 +1,20 @@
+console.log("variables");
+// variables in js
+// var let const ; we can assign vaules in to varibales
+// var is a globally declared.
+var name = `kanaka`;
+
+console.log(name);
+// best way to use variables is using const and let
+{
+  let name = "raju";
+  console.log(name);
+}
+// const => by using const, we cannot reassign the variable and cannot alter the values.
+const You_cant_change_me = 69;
+console.log(You_cant_change_me);
+
+// arrays functions like push and pop work.
+const a = [12, 3, 4, 5];
+a.push(55);
+console.log(a);
